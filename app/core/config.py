b1770b8c243a45c1a475f0b405b4487daf1b7ed2
Maxim Dvoretsky <mx.dvoretsky@gmail.com>
2024-08-01 20:16:08 +0300

    Database Assistant added

--- app/core/config.py
+++ app/core/config.py
@@ -3,9 +3,13 @@ from pydantic_settings import BaseSettings
 
 class DatabaseConfig(BaseModel):
     url: PostgresDsn
+    echo: bool = True
+    echo_pool: bool = False
+    pool_size: int = 5
+    max_overflow: int = 10
 
 class Settings(BaseSettings):
-    db_url: DatabaseConfig
+    db: DatabaseConfig
 
 
 settings = Settings()
